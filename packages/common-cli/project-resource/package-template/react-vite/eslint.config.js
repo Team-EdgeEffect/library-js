9eee6b39a9bef5f825ab978127161e8b900acbc3
hpark <dark1451@gmail.com>
2025-07-21 20:56:19 +0900

    fix: lint

--- packages/common-cli/project-resource/package-template/react-vite/eslint.config.js
+++ packages/common-cli/project-resource/package-template/react-vite/eslint.config.js
@@ -1,10 +1,10 @@
 import js from "@eslint/js";
 import lint from "@locked-dobby/eslint-config";
 import { globalIgnores } from "eslint/config";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+// import reactHooks from "eslint-plugin-react-hooks";
+// import reactRefresh from "eslint-plugin-react-refresh";
 import globals from "globals";
-import tseslint from "typescript-eslint";
+// import tseslint from "typescript-eslint";
 
 export default tseslint.config([
   globalIgnores(["dist"]),
@@ -12,9 +12,12 @@ export default tseslint.config([
     files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
-      tseslint.configs.recommended,
-      reactHooks.configs["recommended-latest"],
-      reactRefresh.configs.vite,
+      /**
+       * - 하단 lint 에서 반영 됩니다.
+       */
+      // tseslint.configs.recommended,
+      // reactHooks.configs["recommended-latest"],
+      // reactRefresh.configs.vite,
     ],
     languageOptions: {
       ecmaVersion: 2020,
--- packages/common-cli/project-resource/package-template/react-vite/src/main.tsx
+++ packages/common-cli/project-resource/package-template/react-vite/src/main.tsx
@@ -1,6 +1,8 @@
+import "./resource/style/index.css";
+
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./resource/style/index.css";
+
 import App from "./component/app";
 
 createRoot(document.getElementById("root")!).render(
